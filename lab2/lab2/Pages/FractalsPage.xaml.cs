ee99a2961fa06063d863a5f6bee92fbfb412e923
AetbaevaJulia <aetbaevajulia05@gmail.com>
2024-10-19 21:46:02 +0500

    add FractalDrawer

--- lab2/lab2/Pages/FractalsPage.xaml.cs
+++ lab2/lab2/Pages/FractalsPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using lab2.Fractals;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace lab2.Pages;
 
@@ -7,5 +10,21 @@ public partial class FractalsPage : Page
     public FractalsPage()
     {
         InitializeComponent();
+    }
+    private void bt1_Click(object sender, RoutedEventArgs e)
+    {
+        //canvas - канва для отрисовки фрактала
+        //tbForD - textBox для введения глубины фрактала
+        //bt1 - кнопка для запуска отрисовки
+        canvas.Children.Clear();
+        int steps = Convert.ToInt32(tbForD.Text);
+        DurersStar fractal = new DurersStar(
+            canvas, new System.Drawing.Point((int)canvas.ActualWidth / 2, (int)canvas.ActualHeight / 2),
+            Brushes.Black, 2, steps);
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
     }
 }
